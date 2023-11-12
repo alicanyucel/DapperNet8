61052c48aebacff7d56f5b408a9b81f9b3d81554
alicanyucel <yucelalican@hotmail.com>
2023-11-12 16:15:03 +0300

    DapperNet8
    .Net Core 8.0 Web Api Dapper Crud Example

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using DapperNet8Example.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperNet8Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeros()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("select * from SuperHero");
+            return Ok(heroes);
+        }
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryFirstAsync<SuperHero>("select * from SuperHero where id=@Id", new {Id=heroId});
+            return Ok(hero);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.ExecuteAsync("insert into SuperHero (Name,FirstName,LastName,Place),values(@Name,@FirstName,@LastName,@Place)",hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("select * from SuperHero");
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.ExecuteAsync("update SuperHero set Name=@Name,FirstName=@FirstNamew,LastName=@LastName,Place=@Place where id=@Id",hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+        [HttpDelete]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int heroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.ExecuteAsync("delete from SuperHero where id=@Id", new {Id=heroId});
+            return Ok(await SelectAllHeroes(connection));
+        }
+    }
+}
---
+++ Models/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DapperNet8Example.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
